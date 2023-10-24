16c996af281f4fdfff838d3a9b18b94004a38cf6
Leonel Alejandro Ceballos Alvarado <lceballos5@ucol.mx>
2023-10-24 13:50:21 -0600

    Version final

--- sources/js/script.js
+++ sources/js/script.js
@@ -3,7 +3,22 @@ function imagenRandom(){
     return nImg;
 }
 
+
+let btnUp = document.getElementById('btnUp');
+btnUp.addEventListener('click', ()=>{
+    document.body.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    })
+
+    document.documentElement.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    })
+})
+
 gsap.from('.navbar .nav_item', {opacity: 0, duration: 0.7, delay: 0.7, y:30, stagger: 0.2})
 gsap.from('.logo', {opacity: 0, duration: 0.7, delay: 0.7, y:10})
 gsap.from('.title', {opacity: 0, duration: 0.7, delay: 0.7, y:30})
 gsap.from('.description', {opacity: 0, duration: 0.7, delay: 0.7, y:30})
+gsap.from('.btnUp', {opacity: 0, duration: 0.7, delay: 0.7, y:30})

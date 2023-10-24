5dff09a0161b018c72ef23a8d310a090bc1edf23
Leonel Alejandro Ceballos Alvarado <lceballos@ucol.mx>
2023-10-24 13:03:28 -0600

    Terminado

--- sources/js/script.js
+++ sources/js/script.js
@@ -1,22 +1,3 @@
-window.onload = () =>{
-    let direccion;
-    let nrandom;
-
-    let arrayImg = ['./sources/img/1.webp', './sources/img/2.webp', './sources/img/3.webp', './sources/img/4.webp'];
-
-    let i1 = document.getElementById('imgOferta1');
-    let i2 = document.getElementById('imgOferta2');
-    let i3 = document.getElementById('imgOferta3');
-    let i4 = document.getElementById('imgOferta4');
-
-    for(let i=0; i<=4; i++){
-        nrandom = imagenRandom();
-        if(nrandom != direccion){
-            i1.setAttribute("src", arrayImg[nrandom]);
-        }
-    }
-}
-
 function imagenRandom(){
     let nImg = Math.round(Math.random(1)*3);
     return nImg;
